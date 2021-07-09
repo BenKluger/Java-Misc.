69d9220d4afa003db2e1d8b060c5e7ac8c81e881
Ben Kluger <69221395+BenKluger@users.noreply.github.com>
2021-07-09 19:42:15 -0400

    Create UnsortedWordList.java

---
+++ Java Projects/Project 2 - 212/UnsortedWordList.java
@@ -0,0 +1,12 @@
+
+public class UnsortedWordList extends WordList {
+
+	// default constructor
+	public UnsortedWordList() { // has super() inside automatically
+	}
+
+	void add(Word w) { // append
+		append(w);
+	}
+
+}
